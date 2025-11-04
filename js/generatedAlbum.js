19733582ea1a5031fbcee64371dc70ee1d387ad5
Andri <example@gmail.com>
2025-11-04 13:48:56 +0200

    lisan failid

---
+++ js/generatedAlbum.js
@@ -0,0 +1,5 @@
+const album = [
+  "images/portrait/pexels-anntarazevich-4850291.jpg",
+  "images/portrait/pexels-aysegul-aytoren-46790226-34567799.jpg",
+  "images/portrait/pexels-ezekixl-6957184.jpg"
+];
